b87b37b9fa1e714071e4e557b8e16c64ce9e6756
tizzoGio <giovani.tizzo@gmail.com>
2023-05-11 10:12:35 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h>React Gio v0</h>
+      </header>
+      <p>
+        Lets bora!
+      </p>
+    </div>
+  );
+}
+
+export default App;
