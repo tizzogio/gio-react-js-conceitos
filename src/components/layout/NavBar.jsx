5eca659326b207ac5fe9e3fe275681ba1ab9af9a
Giovani Tizzo <giovani.tizzo@gmail.com>
2023-05-12 09:28:50 -0300

    test

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
         <ul className={styles.list}>
           <l className={styles.item}><Link to={"/"}>Home</Link></l>
           <li className={styles.item}><Link to={"/empresas"}>Empresa</Link></li>
-          <li className={styles.item}><Link to={"/contatos"}>Contato</Link></li>
+          <li className={styles.item}><Link to={"/contatos"}>Contatos</Link></li>
           <li className={styles.item}><Link to={"/exemplos"}>Exemplos</Link></li>
         </ul>
 
